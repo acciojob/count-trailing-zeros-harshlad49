c0ae753dcde777998e41f6c63d9b7aa5222c7aae
Harsh Lad <91070567+harshlad49@users.noreply.github.com>
2025-06-08 08:33:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 function trailingZeros(n) {
-  function trailingZeros(n) {
-    let count = 0;
+      let count = 0;
     while (n >= 5) {
         n = Math.floor(n / 5);
         count += n;
     }
     return count;
-}
-
-// Get user input and display result
-let input = parseInt(prompt("Enter a non-negative integer:"));
-let result = trailingZeros(input);
-alert("Number of trailing zeros in " + input + "! is: " + result);
-
 }
 
 const input = prompt("Enter a number");
